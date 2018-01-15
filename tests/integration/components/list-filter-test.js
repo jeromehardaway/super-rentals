e82e3e7a15d6f348d19d59cdc954877ada076cce
Jerome Hardaway <jerome@vetswhocode.io>
2018-01-14 20:57:24 -0600

    correct tests in listing. Globalized return was pushing an error.

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -28,10 +28,36 @@ test ('should initially load all listings', function (assert){
     {{/list-filter}}
   `);
 
+test('should update with matching listings', function (assert) {
+  this.on('filterByCity', (val) => {
+    if (val === '') {
+      return RSVP.resolve({
+        query: val,
+        results: ITEMS });
+    } else {
+      return RSVP.resolve({
+        query: val,
+        results: FILTERED_ITEMS });
+    }
+  });
+
+  this.render(hbs`
+    {{#list-filter filter=(action 'filterByCity') as |results|}}
+      <ul>
+      {{#each results as |item|}}
+        <li class="city">
+          {{item.city}}
+        </li>
+      {{/each}}
+      </ul>
+    {{/list-filter}}
+  `);
+
+  // The keyup event here should invoke an action that will cause the list to be filtered
   this.$('.list-filter input').val('San').keyup();
 
-  return wait().then(()=> {
+  return wait().then(() => {
     assert.equal(this.$('.city').length, 1);
     assert.equal(this.$('.city').text().trim(), 'San Francisco');
-    });
   });
+});
